4010e9f5272db5c911d38091b2869bf30db7427b
paul lahana <paul.lahana@gmail.com>
2019-11-25 17:28:57 +0100

    devise and users parameters

---
+++ db/migrate/20191125162147_add_details_to_users.rb
@@ -0,0 +1,8 @@
+class AddDetailsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :address, :string
+    add_column :users, :level, :integer, default: 1
+  end
+end
