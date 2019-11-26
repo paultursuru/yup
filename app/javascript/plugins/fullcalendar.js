972bdacc54e8a50d15dc05b40c15afcef523a32e
paul lahana <paul.lahana@gmail.com>
2019-11-26 18:23:41 +0100

    fixed the listed calendar to show description

--- app/javascript/plugins/fullcalendar.js
+++ app/javascript/plugins/fullcalendar.js
@@ -14,6 +14,14 @@ const calendar = () => {
           console.log(info.event.extendedProps.todo_id);
           // Creer un lien et y passer l'id de la todo à marquer comme done.
           // "<a href="/todos/${}/mark_as_done">Mark as done !</a>"
+        },
+        eventRender: function(info) {
+          const titleDiv = info.el.querySelector('.fc-list-item-title');
+          titleDiv.parentElement.firstChild.remove();
+          // const eventRow = info.el.querySelector('.fc-list-item')
+          const eventRow = titleDiv.parentElement;
+          info.el.insertAdjacentHTML('beforeend', `<td class='fc-list-item-title fc-widget-content'>${info.event.extendedProps.description}</td>`);
+          // eventRow.append(`<td class='fc-list-item-title fc-widget-content'>${info.event.extendedProps.description}</td>`);
         }
       });
 
