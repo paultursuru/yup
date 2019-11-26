fe41dad01add01dff0fab7cda1b963c7e2c77e79
Astrid de Boysson <astriddeboysson@gmail.com>
2019-11-26 16:31:51 +0100

    ok

---
+++ app/controllers/planters_controller.rb
@@ -0,0 +1,2 @@
+class PlantersController < ApplicationController
+end
---
+++ test/controllers/planters_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PlantersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
