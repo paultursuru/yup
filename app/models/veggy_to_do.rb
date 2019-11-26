03358768dba1a825f6c75c52dc18b0e45be7f1f8
paul lahana <paul.lahana@gmail.com>
2019-11-26 10:28:30 +0100

    changes done on navbar and general layout

--- app/models/veggy_to_do.rb
+++ app/models/veggy_to_do.rb
@@ -3,13 +3,22 @@
 # Table name: veggy_to_dos
 #
 #  id                   :bigint           not null, primary key
-#  to_do_template_id    :bigint
-#  periodicity          :integer
-#  initial              :boolean
 #  after_planting_delay :integer
+#  initial              :boolean
+#  periodicity          :integer
 #  created_at           :datetime         not null
 #  updated_at           :datetime         not null
+#  to_do_template_id    :bigint
 #  veggy_id             :bigint
+#
+# Indexes
+#
+#  index_veggy_to_dos_on_to_do_template_id  (to_do_template_id)
+#  index_veggy_to_dos_on_veggy_id           (veggy_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (to_do_template_id => to_do_templates.id)
 #
 
 class VeggyToDo < ApplicationRecord
