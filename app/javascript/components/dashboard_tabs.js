0e5aeaba347c2deb1e8e1ab830f0fbdf4cdc938c
Mathias Thibaud <math.thibaud@gmail.com>
2019-11-27 12:17:12 +0100

    fixed js

--- app/javascript/components/dashboard_tabs.js
+++ app/javascript/components/dashboard_tabs.js
@@ -1,18 +1,24 @@
 function hideAllTabContents () {
   document.querySelectorAll('.tab-content').forEach((tabContent) => {
     tabContent.classList.add('d-none')
-    // tabContent.classList.remove('active')
   })
-}
+};
+
+function deactiveTabs () {
+  document.querySelectorAll('.tab').forEach((tab) => {
+    tab.classList.remove('active')
+  })
+};
 
 function initiateDashboardTab () {
   if (document.querySelector(".dashboard")) {
     document.querySelectorAll('.tab').forEach((tab) => {
       tab.addEventListener('click', function(event){
+        deactiveTabs();
         hideAllTabContents()
         let selector = `#${event.currentTarget.id}-content`
         document.querySelector(selector).classList.remove('d-none')
-        // document.querySelector(selector).classList.add('active')
+        event.currentTarget.classList.add('active')
       })
     })
   }
