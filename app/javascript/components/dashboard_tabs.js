a0684c43da71e3ec87a301054fd3d054b694d0bf
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2019-11-27 15:59:27 +0100

    customize-calendar ok

--- app/javascript/components/dashboard_tabs.js
+++ app/javascript/components/dashboard_tabs.js
@@ -1,3 +1,9 @@
+function hideOtherTabContents () {
+  document.querySelectorAll('.other').forEach((tabContent) => {
+    tabContent.classList.add('d-none')
+  })
+};
+
 function hideAllTabContents () {
   document.querySelectorAll('.tab-content').forEach((tabContent) => {
     tabContent.classList.add('d-none')
@@ -11,6 +17,7 @@ function deactiveTabs () {
 };
 
 function initiateDashboardTab () {
+  hideOtherTabContents()
   if (document.querySelector(".dashboard")) {
     document.querySelectorAll('.tab').forEach((tab) => {
       tab.addEventListener('click', function(event){
