4485ed5cee7e49656ca6de4b68541f1016b4f221
Mathias Thibaud <math.thibaud@gmail.com>
2019-11-27 12:14:58 +0100

    in progress

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,9 @@
 import "bootstrap";
-import calendar from '../plugins/fullcalendar.js';
+import calendar from '../plugins/fullcalendar';
 import '@fullcalendar/core/main.css';
 import '@fullcalendar/daygrid/main.css';
 import '@fullcalendar/list/main.css';
-import { initiateDashboardTab } from '../components/dashboard_tabs.js'
+import { initiateDashboardTab } from '../components/dashboard_tabs'
 
 calendar();
 initiateDashboardTab();
