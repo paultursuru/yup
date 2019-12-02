6bc5570abdfdcbaef1c18a7c8ff36b24257bc9d0
paul lahana <paul.lahana@gmail.com>
2019-12-02 08:26:04 +0100

    made the logo bigger in the navbar and changed some display in cards and calendar

--- app/models/planter.rb
+++ app/models/planter.rb
@@ -25,4 +25,7 @@ class Planter < ApplicationRecord
   has_many :planted_veggies, dependent: :destroy
   has_many :veggies, through: :planted_veggies
   has_many :to_dos, through: :planted_veggies
+  validates :name, presence: true
+  validates :orientation, presence: true
+  validates :size, presence: true
 end
