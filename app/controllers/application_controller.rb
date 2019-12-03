41ff3b804232026a913b246098fc3aff8a11965a
Astrid de Boysson <astriddeboysson@gmail.com>
2019-12-03 18:00:08 +0100

    time optimisation- fewer requests executed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_order
   # protect_from_forgery with: :exception
   # before_action :set_order
 
@@ -26,5 +27,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     dashboard_path
+  end
+
+  def set_order
+    @current_order = current_user.orders.find_by(state: 'pending') if user_signed_in?
   end
 end
